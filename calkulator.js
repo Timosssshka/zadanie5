62c10a3d687d6d7ff1363c8946a551fbbbee7a19
Timosssshka <112942954+Timosssshka@users.noreply.github.com>
2022-10-11 02:05:00 +0300

    Update calkulator.js

--- calkulator.js
+++ calkulator.js
@@ -1,1 +1,20 @@
-
+  window.addEventListener('DOMContentLoaded', function (event) {
+    console.log("DOM fully loaded and parsed");
+    let b = document.getElementById("my-button");
+    b.addEventListener("click", proiz);
+  });
+    function proiz() {
+      const check = /^\d*\.?\d+$/;
+    let f1 = document.getElementsByName("p1");
+    let f2 = document.getElementsByName("p2");
+      if (check.test(f1[0].value) == false || check.test(f2[0].value) == false || f1[0].value == 0 || f2[0].value == 0) {
+        alert("Введите корректно!");    
+    }
+      else
+      {
+        f1[0]=Number.parseInt(f1[0])
+        f2[0]=Number.parseInt(f2[0]);
+        var a = f1[0].value * f2[0].value;
+        alert("Ваш ответ: "+ a);
+      }
+    }
